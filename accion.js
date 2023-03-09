920e8184bbf5d5116114266455d51aefeace5e4c
ElLokoHernan <85263208+ElLokoHernan@users.noreply.github.com>
2023-03-08 21:29:14 -0300

    Primer

---
+++ accion.js
@@ -0,0 +1,33 @@
+//animacion de foto
+function bigImg(x) {
+  x.style.height = "100px";
+  x.style.width = "auto";
+
+}
+
+function normalImg(x) {
+  x.style.height = "350px";
+  x.style.width = "autopx";
+}
+
+
+//menu
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+  //ocultar
+  function ocultar(){
+    document.getElementById('contacto').style.display='none'
+  }
+ //mostrar 
+ function mostrarEdu(){
+    document.getElementById('Educacion').style.display='flex'
+  }
+  function mostrarTrab(){
+    document.getElementById('Trabajo').style.display='flex'
+  }
+
+  function mostrarPerf(){
+    document.getElementById('Perfil').style.display='flex'
+  }
